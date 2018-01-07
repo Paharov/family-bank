ed73aa03c5fb9f3003dd23b2c84c44e59bfda737
Andras_Szanto <Andras_Szanto@epam.com>
2018-01-07 20:57:28 +0100

    Create a bank bean

--- src/main/java/com/epam/training/homework/familybank/spring/SpringConfigurationService.java
+++ src/main/java/com/epam/training/homework/familybank/spring/SpringConfigurationService.java
@@ -1,11 +1,22 @@
 package com.epam.training.homework.familybank.spring;
 
+import com.epam.training.homework.familybank.domain.Account;
 import com.epam.training.homework.familybank.service.PopulateDatabaseService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.math.BigDecimal;
+
 @Configuration
 public class SpringConfigurationService {
+
+    @Bean
+    public Account bank() {
+        Account bank = new Account();
+        bank.setAssets(new BigDecimal(0));
+        bank.setDebts(new BigDecimal(0));
+        return bank;
+    }
 
     @Bean(initMethod = "populateDatabase")
     public PopulateDatabaseService populateDatabaseService() {
